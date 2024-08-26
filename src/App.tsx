051c62d76728567a548410cd4a9e89910837cd0b
DanNilloOli <danillooliveirasp@gmail.com>
2024-08-26 09:48:37 -0300

    React

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+
 
 function App() {
-
+  //código typescript
+  
+ 
   return (
-
-<> 
-
-      <Home />
-
+    //o return tem oq vai aparecer na tela
+    //codigo tsx equivalente a html e css
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element ={<Cadastro />}/>
+          <Route path="/home" element ={<Home />}/>
+          </Routes>
+        </div>
+      <Footer />
+      </BrowserRouter>
     </>
+
+
   )
 }
 
